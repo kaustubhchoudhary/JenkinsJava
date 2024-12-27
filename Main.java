2386d6474a47a325034e58e17fa1e80becb66109
kaustubhchoudhary <kaustubhchoudhary@yahoo.com>
2024-12-27 10:10:54 +0530

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+	
+	public static void main(String[] args){
+		System.out.println("\n Testing jenkins with git");
+		System.out.println("\n Testing 1 minute executions");
+		
+		Student.saysHello();
+	}
+}
+
+class Student {
+	
+	public static void saysHello (){
+		System.out.println("\n Student says hello");
+	}
+	
+}
